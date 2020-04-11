de897ee27c56aefea723c1210c1713d68bba102b
Vishrut Reddi <vishrutreddi@gmail.com>
2020-04-11 16:32:41 -0700

    Updates

---
+++ precache-manifest.47bf3973e0c02ebe960e190ae5d11f0c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c074b260d1c9067d72a0baec7646f713",
+    "url": "/bottle-web/index.html"
+  },
+  {
+    "revision": "00a822ecab958deeff28",
+    "url": "/bottle-web/static/css/main.f4251c63.chunk.css"
+  },
+  {
+    "revision": "41836297c483f2877e32",
+    "url": "/bottle-web/static/js/2.195a33a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bottle-web/static/js/2.195a33a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00a822ecab958deeff28",
+    "url": "/bottle-web/static/js/main.0dd540e4.chunk.js"
+  },
+  {
+    "revision": "358775646a6f592d37f1",
+    "url": "/bottle-web/static/js/runtime-main.6584961b.js"
+  },
+  {
+    "revision": "c6b7ba1dc1c80e00e6c17ac72eb753cc",
+    "url": "/bottle-web/static/media/BlackBerryJam.c6b7ba1d.ttf"
+  },
+  {
+    "revision": "ffcd876c5561db066c09f5922b384473",
+    "url": "/bottle-web/static/media/video.ffcd876c.mp4"
+  }
+]);
