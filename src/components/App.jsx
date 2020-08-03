9e2fb6d34387e4f23e624689e03f4a46ec4ceac8
Vishrut Reddi <vishrutreddi@gmail.com>
2020-08-02 17:17:03 -0700

    Add Success page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './home/home';
 import { Shop } from './shop/shop';
+import Success from './success/success';
 
 const App = () => (
   <Switch>
     <Route path="/" component={Home} exact />
     <Route path="/shop" component={Shop} />
+    <Route path="/success" component={Success} />
   </Switch>
 );
 
